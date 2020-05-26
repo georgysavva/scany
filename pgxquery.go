2b244b0c2b03760b8cba7b7ce88a3a2b1f14284b
georgysavva <georgy.savva@gmail.com>
2020-05-26 17:35:49 +0300

    Implement reflection package that can scan rows into slice, map, struct and primitive time.

--- pgxquery.go
+++ pgxquery.go
@@ -2,6 +2,8 @@ package pgxquery
 
 import (
 	"context"
+	"github.com/georgysavva/pgxquery/internal/reflection"
+
 	"github.com/jackc/pgx/v4"
 	"github.com/pkg/errors"
 )
@@ -15,7 +17,7 @@ func QueryAll(ctx context.Context, q QueryI, dst interface{}, sql string, args .
 	if err != nil {
 		return errors.Wrap(err, "query rows")
 	}
-	err = scanRows(dst, rows, false /* exactlyOneRow */)
+	err = Scan(dst, rows)
 	return errors.WithStack(err)
 }
 
@@ -24,12 +26,17 @@ func QueryOne(ctx context.Context, q QueryI, dst interface{}, sql string, args .
 	if err != nil {
 		return errors.Wrap(err, "query rows")
 	}
-	err = scanRows(dst, rows, true /* exactlyOneRow */)
+	err = ScanOne(dst, rows)
 	return errors.WithStack(err)
 }
 
-func ScanRows(dst interface{}, rows pgx.Rows) error {
+func Scan(dst interface{}, rows pgx.Rows) error {
 	err := scanRows(dst, rows, false /* exactlyOneRow */)
+	return errors.WithStack(err)
+}
+
+func ScanOne(dst interface{}, rows pgx.Rows) error {
+	err := scanRows(dst, rows, true /* exactlyOneRow */)
 	return errors.WithStack(err)
 }
 
@@ -42,16 +49,24 @@ var notFoundErr = errors.New("no row was found")
 
 func scanRows(dst interface{}, rows pgx.Rows, exactlyOneRow bool) error {
 	defer rows.Close()
+	dstRef, err := reflection.ParseDst(dst, exactlyOneRow)
+	if err != nil {
+		return errors.WithStack(err)
+	}
+	scanner := &reflection.RowsWrapper{rows}
+
 	var rowsAffected int
 	for rows.Next() {
-		if err := rows.Scan(dst); err != nil {
-			return errors.Wrap(err, "scan rows")
+		if err := dstRef.Fill(scanner); err != nil {
+			return errors.WithStack(err)
 		}
 		rowsAffected++
 	}
+
 	if err := rows.Err(); err != nil {
 		return errors.Wrap(err, "rows final error")
 	}
+
 	if exactlyOneRow {
 		if rowsAffected == 0 {
 			return errors.WithStack(notFoundErr)
