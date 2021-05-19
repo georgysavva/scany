d28f8d85a1bd4b5684e80d79dd3cbab5dce153af
Georgy Savva <georgy.savva@gmail.com>
2021-05-19 11:10:15 +0300

    Add notice about single Row type support to dbscan docs

--- dbscan/doc.go
+++ dbscan/doc.go
@@ -1,7 +1,7 @@
 // Package dbscan allows scanning data from abstract database rows into Go structs and more.
 /*
 dbscan works with abstract Rows interface and doesn't depend on any specific database or a library.
-If a type implements Rows it can leverage the full functionality of this package.
+If a type implements Rows, it can leverage the full functionality of this package.
 
 Mapping struct field to database column
 
@@ -13,19 +13,19 @@ The main feature of dbscan is the ability to scan rows data into structs.
 		Email     string
 	}
 
-	// Query rows from the database that implement dbscan.Rows interface.
+	// Query rows from the database that implements dbscan.Rows interface.
 	var rows dbscan.Rows
 
 	var users []*User
 	dbscan.ScanAll(&users, rows)
 	// users variable now contains data from all rows.
 
-By default, to get the corresponding database column dbscan translates struct field name to snake case.
+By default, to get the corresponding database column, dbscan translates the struct field name to snake case.
 To override this behavior, specify the column name in the `db` field tag.
 In the example above User struct is mapped to the following columns: "user_id", "first_name", "email".
 
-If selected rows contain a column that doesn't have a corresponding struct field dbscan returns an error,
-this forces to only select data from the database that application needs.
+If selected rows contain a column that doesn't have a corresponding struct field, dbscan returns an error,
+this forces to only select data from the database that the application needs.
 
 dbscan supports commas "," in the struct tag name.
 That makes it compatible with the struct tag formats of other libraries.
@@ -34,11 +34,11 @@ So `db:"user_id,other_tag_value"` struct tag is equivalent to `db:"user_id"` for
 
 Reusing structs
 
-dbscan works recursively, a struct can contain embedded or nested structs as well.
+dbscan works recursively. A struct can contain embedded or nested structs as well.
 It allows reusing models in different queries. Structs can be embedded or nested both by value and by a pointer.
 If you don't specify the `db` tag, dbscan maps fields from nested structs to database columns
-with the struct field name translated to snake case as the prefix,
-on the opposite, fields from embedded structs are mapped to database columns without any prefix.
+with the struct field name translated to snake case as the prefix. 
+On the opposite, fields from embedded structs are mapped to database columns without any prefix.
 dbscan uses "." to separate the prefix. Here is an example:
 
 	type UserPost struct {
@@ -106,15 +106,15 @@ and if the database library supports a type, dbscan supports it automatically, f
 		// Any fields that this custom type needs
 	}
 
-User struct is valid and every field will be scanned properly, the only condition for this
+User struct is valid, and every field will be scanned correctly, the only condition for this
 is that your database library can handle *string, CustomNullInt, CustomData and *CustomData types.
 
 Ignored struct fields
 
-In order for dbscan to work with a field it must be exported, unexported fields will be ignored.
-The only exception is embedded structs, the type that is embedded might be unexported.
+In order for dbscan to work with a field, it must be exported. Unexported fields will be ignored.
+The only exception is embedded structs. The type that is embedded might be unexported.
 
-It's possible to explicitly mark a field as ignored for dbscan. To do this set `db:"-"` struct tag.
+It's possible to mark a field as ignored for dbscan explicitly. To do this set `db:"-"` struct tag.
 By the way, it works for nested and embedded structs as well, for example:
 
 	type Comment struct {
@@ -164,7 +164,7 @@ Scanning into map
 Apart from scanning into structs, dbscan can handle maps,
 in that case, it uses database column name as the map key and column data as the map value, for example:
 
-	// Query rows from the database that implement dbscan.Rows interface.
+	// Query rows from the database that implements dbscan.Rows interface.
 	var rows dbscan.Rows
 
 	var results []map[string]interface{}
@@ -172,7 +172,7 @@ in that case, it uses database column name as the map key and column data as the
 	// results variable now contains data from all rows.
 
 Map type isn't limited to map[string]interface{},
-it can be any map with a string key, e.g. map[string]string or map[string]int,
+it can be any map with a string key, e.g., map[string]string or map[string]int,
 if all column values have the same specific type.
 
 Scanning into other types
@@ -180,7 +180,7 @@ Scanning into other types
 If the destination isn't a struct nor a map, dbscan handles it as a single column scan,
 dbscan ensures that rows contain exactly one column and scans destination from that column, for example:
 
-	// Query rows from the database that implement dbscan.Rows interface.
+	// Query rows from the database that implements dbscan.Rows interface.
 	var rows dbscan.Rows
 
 	var results []string
@@ -189,14 +189,20 @@ dbscan ensures that rows contain exactly one column and scans destination from t
 
 Duplicate columns
 
-Rows must not contain duplicate columns otherwise dbscan won't be able to decide
+Rows must not contain duplicate columns otherwise, dbscan won't be able to decide
 from which column to select and will return an error.
+
+Support for Row type
+
+dbscan doesn't support a single row type like Row, which you might see in many database libraries.
+This is because the Row type doesn't expose the required information to do the mapping between columns and the Go destination.
+So dbscan can only work with Rows type, and it provides a convenient function ScanOne to handle a single row case; see ScanOne for details.
 
 Rows processing
 
 ScanAll and ScanOne functions take care of rows processing,
 they iterate rows to the end and close them after that.
-Client code doesn't need to bother with that, it just passes rows to dbscan.
+Client code doesn't need to bother with that. It just passes rows to dbscan.
 
 Manual rows iteration
 
@@ -205,9 +211,8 @@ see RowScanner for details.
 
 Implementing Rows interface
 
-dbscan can be used with any database library that has a concept of rows and can implement dbscan Rows interface.
-It's pretty likely that your rows type already implements Rows interface as-is,
-for example this is true for the standard *sql.Rows type.
-Or you just need a thin adapter how it was done for pgx.Rows in pgxscan, see pgxscan.RowsAdapter for details.
+dbscan can be used with any database library with a concept of rows and can implement dbscan Rows interface.
+It's pretty likely that your rows type already implements the Rows interface as-is. For example, this is true for the standard *sql.Rows type.
+Or you just need a thin adapter as it is done for pgx.Rows in pgxscan, see pgxscan.RowsAdapter for details.
 */
 package dbscan
